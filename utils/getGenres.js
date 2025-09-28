3a7fa7620358c35bad2173f05b08a53694b24c7e
Yarlin <yarlinlynn@gmail.com>
2025-09-28 16:54:31 +0200

    Add jsdocs for genresDropDown function

--- utils/getGenres.js
+++ utils/getGenres.js
@@ -13,7 +13,13 @@ export function getGenreTitles(podcast) {
     .filter(title => title !== "Unknown");
 }
 
-
+/** 
+ * Generate a unique list of genre options for use in a dropdown.
+ * Combines all genres from the given podcasts, maps their IDs to titles
+ * @param {Array<Object>} podcasts - Array of podcast objects fetched from the API.
+ * Each podcast is expected to have a `genres` array containing numeric genre IDs.
+ * @returns {string[]} A list of genre titles including "All" at the start.
+*/
 export function genresDropDown(podcasts) {
   const genres = ['All', ...new Set(podcasts.flatMap(podcast => getGenreTitles(podcast)))]
   return genres;
