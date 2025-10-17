57c093fc35eaea5105495dc3762d21552c74b5c4
Yarlin <yarlinlynn@gmail.com>
2025-10-17 12:56:41 +0200

    Add environment variable to deploy project to vercel

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || '/YARSTR25495_FTO2505-B_Yarlin-Struis_DJSPP'
 })
